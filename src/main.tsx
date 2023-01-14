d70b0081f4c4c7e2cdac894feb8129d0d6a5596c
rossilor95 <rossilor95@outlook.it>
2023-01-14 10:54:28 +0100

    chore: first commit

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TodoDemoApp from './components/TodoDemoApp';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+        <TodoDemoApp />
+    </React.StrictMode>,
+);
