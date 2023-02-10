f9ce420ba15e37b41919ef11d25f2fb1ff98a4ec
rossilor95 <rossilor95@outlook.it>
2023-02-10 14:52:27 +0100

    feat: implement conditional rendering of NewTodoForm

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import TodoTable from './TodoTable';
-import { Box } from '@mui/material';
+import { Box, Button, Stack } from '@mui/material';
 import Header from './Header';
 import { useState } from 'react';
 import NewTodoForm from './NewTodoForm';
@@ -13,6 +13,8 @@ const App = () => {
     { id: nanoid(), description: 'Make dinner', assigned: 'User One' },
     { id: nanoid(), description: 'Charge phone battery', assigned: 'User One' },
   ]);
+
+  const [isFormVisible, setFormVisible] = useState(false);
 
   const addTodo = (assigned: string, description: string) => {
     const newTodo: Todo = {
@@ -34,14 +36,16 @@ const App = () => {
         <Stack spacing={4}>
           <TodoTable todos={todos} deleteTodo={deleteTodo}/>
           <Stack flexDirection="row">
-            <Button variant="contained">
-              Add new todo
+            <Button variant="contained" onClick={() => setFormVisible(!isFormVisible)}>
+              {
+                isFormVisible ? "Close form" : "Open form"
+              }
             </Button>
           </Stack>
         </Stack>
-        <NewTodoForm
-          addTodo={addTodo}
-        />
+        {
+          isFormVisible && <NewTodoForm addTodo={addTodo}/>
+        }
       </Box>
     </>
   );
